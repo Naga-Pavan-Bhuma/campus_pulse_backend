c054053930eba84fcf705f719502fb9ca8ede6cc
Naga-Pavan-Bhuma <bhumanagapavan@gmail.com>
2025-04-19 23:03:16 +0530

    small change

--- src/index.js
+++ src/index.js
@@ -39,25 +39,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use('/academiccalendar', academicCalendarRouter);
 // Routes
-<<<<<<< HEAD
-
 app.use("/", clubRouter);
 app.use("/", announcementRouter);
 app.use('/', authRoutes);
 app.use("/", facultyRoutes);
 app.use("/", messMenuRouter);
 
-=======
-try {
-  app.use("/", clubRouter);
-  app.use("/", announcementRouter)
-  app.use('/', authRoutes);
-  app.use("/", messMenuRouter);
-  app.use('/academiccalendar', academicCalendarRouter);
-}catch (err) {
-  console.error("Error while setting up routes:", err.message);
-}
->>>>>>> 15f46f8 (Academic Calendar added)
 // Use the appropriate routes
 app.use('/timetable', timetableRouter); // Original timetable routes
 app.use('/faculty-timetable', facultyTimetableRouter); // New route for faculty timetable
