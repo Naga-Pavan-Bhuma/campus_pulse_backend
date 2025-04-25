355ca53ca0194a47454235767a34ed2f7733ed9d
Anirudh-654 <anirudhluky654@gmail.com>
2025-04-25 08:19:45 +0530

    Webcam added

--- src/models/announcements.js
+++ src/models/announcements.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const announcementSchema = new mongoose.Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
-    postedBy: { type: String }, // Can be Faculty or Admin
+    postedBy: { type: String },
     userType: { type: String, enum: ["Faculty", "Admin"], required: true },
     audience: {
         type: [String],
