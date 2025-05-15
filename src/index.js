a5c3f4dba4bd6d9dc65ef739a687e67c7db6d190
Naga-Pavan-Bhuma <bhumanagapavan@gmail.com>
2025-05-15 19:27:20 +0530

    Updated to use process.env.PORT for Render deployment

--- src/index.js
+++ src/index.js
@@ -64,9 +64,10 @@ app.get("/user", async (req, res) => {
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB successfully');
-    app.listen(5000, () => {
-      console.log('Server running on port 5000');
-    });
+    const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
   })
 
   .catch(err => {
